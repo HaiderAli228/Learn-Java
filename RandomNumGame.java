312ae764b240e66220afa5e38bb58660f3a24590
HaiderAli228 <flutter2830@gmail.com>
2025-05-14 12:56:59 +0500

    ..

--- RandomNumGame.java
+++ RandomNumGame.java
@@ -1,5 +1,10 @@
+import java.util.*;
+
 public class RandomNumGame {
     public static void main(){
         System.out.println("Welcome To Random Number Game");
+        Random obj = new Random();
+
+        int randomNum = obj.nextInt(10) ;
     }
 }
